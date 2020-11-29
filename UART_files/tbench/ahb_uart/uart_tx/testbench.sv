8f3633fc04415d445a380e169deb3495a5852dc6
Ethan Sarif-Kattan <es5017@ee-mill3.ee.ic.ac.uk>
2020-11-29 19:58:57 +0000

    untested full unit testbench for uart_tx

--- UART_files/tbench/ahb_uart/uart_tx/testbench.sv
+++ UART_files/tbench/ahb_uart/uart_tx/testbench.sv
@@ -4,11 +4,27 @@ module testbench;
     reg clk;
     reg baud_tick;
 
-    _if _if(clk, baud_tick);
+    _if intf(clk, baud_tick);
 
-    
+    UART_TX DUT (
+        .clk(clk),
+        .resetn(intf.rst_n),
+        .tx_start(intf.tx_start),
+        .b_tick(baud_tick),
+        .d_in(intf.d_in),
+        .tx_done(intf.tx_done),
+        .tx(intf.tx)
+    );
 
     initial begin
-        
+        clk <= 0;
+        intf.resetn <= 0;
+        intf.tx_start <= 0;
+
+        test t = new;        
+        t.env.vif = intf;
+        t.run();
+
+        #1000 $finish;
     end
 endmodule

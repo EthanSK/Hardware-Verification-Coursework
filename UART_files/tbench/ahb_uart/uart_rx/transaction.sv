34e73fe5c22aefa49aa467be167c457e1cbc3e88
Ethan Sarif-Kattan <es5017@ee-mill3.ee.ic.ac.uk>
2020-12-08 16:50:54 +0000

    yes

--- UART_files/tbench/ahb_uart/uart_rx/transaction.sv
+++
@@ -1,9 +0,0 @@
-class uart_rx_transaction;
-
-    rand bit [8:0] d_in; //serial data coming in
-    bit [8:0] d_out;  //vector data going out
- 
-    function void print(string tag="");
-        $display ("T=%0t [Transaction] (Tag: %s) d_in=0x%0h d_out=0x%0h", $time, tag, d_in, d_out);
-    endfunction
-endclass
--- UART_files/tbench/ahb_uart/uart_tx/interface.sv
+++
@@ -1,7 +0,0 @@
-interface uart_tx_if(input bit clk, input bit baud_tick); //can't be called if
-    logic                resetn;
-    logic                tx_start;
-    logic [8:0]          d_in;
-    logic                tx_done;
-    logic                tx;
-endinterface
